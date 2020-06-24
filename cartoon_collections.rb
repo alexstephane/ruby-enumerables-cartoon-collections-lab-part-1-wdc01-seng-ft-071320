79604c622d141fbf0d00b9ca713a34a6141843a2
Alex Ngundji <alexngundji@gmail.com>
2020-06-24 15:05:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,8 @@ end
 
 def list_dwarves(array)
   
-  #colors = [:red, :blue, :black].each.with_index(1) do |elem, i|
+  #colors = [:red, :blue, :black]
+  array.each.with_index(1) do |elem, i|
 
     puts "#{i}: #{elem}"
   end
