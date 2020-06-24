eaeaac9f8c2528671a99e6c7d57f06bea50cb94f
Alex Ngundji <alexngundji@gmail.com>
2020-06-24 15:03:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,7 @@ end
 
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  
   colors = [:red, :blue, :black].each.with_index(1) do |elem, i|
 
     puts "#{i}: #{elem}"
